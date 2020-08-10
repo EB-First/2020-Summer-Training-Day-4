0d0cb039bf7bf398b85323749727733fb76bfad8
siddharthlohani05 <68655332+siddharthlohani05@users.noreply.github.com>
2020-08-10 18:01:36 -0400

    finished part 1

---
+++ Day4Assignment Part1/Day4Best/src/main/java/frc/robot/commands/GetCounts.java
@@ -0,0 +1,46 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.RobotContainer;
+import frc.robot.subsystems.SensorSubsystem;
+
+public class GetCounts extends CommandBase {
+  /**
+   * Creates a new GetCounts.
+   */
+  public GetCounts() {
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(RobotContainer.m_SensorSubsystem);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    RobotContainer.m_SensorSubsystem.getCounts();
+
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    RobotContainer.m_SensorSubsystem.stopMotors();
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
